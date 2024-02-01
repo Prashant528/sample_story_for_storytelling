9d8bc978f95153cda2613049ca119d26f2daf567
Prashant528 <prashant.tandan528@gmail.com>
2024-01-31 18:04:36 -0800

    Making the story more relatable to the actual process. Iter 1

---
+++ docs/story_backups/backup_jan_31.js
@@ -0,0 +1,136 @@
+const storyPages = [
+    // Each element of this array represents a page in the story
+    `<p> 
+        This is a story about the process of making a contribution to the OSS projects.
+        <br><br><br> Click next to start the story...
+        </p>
+    `,
+    `Page:1
+    <p> 
+        Once upon a time in the mystical land, there lived a curious and determined girl named Kitty who was in love with books, 
+        magic and mythical creatures. 
+        One day, she stumbled upon a fascinating book about dragons, the secrets of the code-knitters and their magical spells. 
+        Intrigued by the prospect of summoning her own <b>ice-dragon</b>, Kitty delved into the world of <b>code-knitters</b>.
+        `,
+        `
+        Page:2
+        <p>
+            The code-knitters, a unique species, possessed the power to create <b>spells</b> and summon magnificent dragons. 
+            They lived harmoniously in their tiny kingdoms called "<b>Codelands</b>", knitting the spells together.  
+            Kitty also learned that these code-knitters were always on 
+            the lookout for new members to join their ranks and contribute to the summoning 
+            of even mightier dragons. Each Codeland had a special book called <b>The Golden Book</b>. It was not enough for a code-knitter
+             to create new magical spells to summon a dragon. They could summon one only if they were
+             able to add their magic spell to the Golden Book of the Codeland.
+        </p>
+        </p>`,
+        `
+        Page:3
+        
+
+        <p>
+            As she dived deeper and deeper into the book, she became more and more eager to summon a pet dragon of herself.
+            She realized that she now had to become a citizen of one of the <b>Codeland</b> kingdoms soon.
+            </p>
+            <p>
+             She traveled through the Codelands, observing the dragons they were summoning. As she had guessed, it was hard 
+             to find Codelands that summoned ice dragons like she wanted. After a long and tiring journey, she decided to rest
+             on the top of a hill. Little did she know that she was in for a pleasant surprise.
+             </p>
+             <p>
+             She was flabbergasted by what she could saw from there -- there were code-knitters riding huge ice dragons and
+             circling a fortified city full of hustle and bustle. She could see the whole <b>layout</b> of the city where the
+             code-knitters were assembling in groups and working furiously to summon bigger dragons.
+             After all the hardships, she had finally found her Codeland.
+
+        </p>`,
+        `
+        Page:4
+        <p> 
+            Kitty was excited and wasted no time to explore the Codeland. All of the code-knitters were friendly and
+            she was given a <b>handbook</b> that she read to find helpful information about the Codeland and the dragons.
+            She found out that the code-knitters had a tradition of gathering in large <b>forums</b> to discuss 
+            spells, dragons, and other magical matters. Though warmly welcomed into the group, 
+            Kitty found it a bit challenging to grasp the complexities of their discussions. 
+            Undeterred, she decided to navigate the new kingdom, acknowledging that some 
+            tasks would indeed be complex for her.
+        </p>
+        `,
+    `Page:5
+    <p>
+        Determined, Kitty sought the guidance of a <b>mentor</b> who taught new citizens. 
+        He  welcomed her and showed her the treasured Golden Codebook of the Codeland.
+        He also provided insights into the <b>history of the codebook</b>
+        and the <b>behaviors expected</b> in the kingdom. He emphasized the importance of
+        understanding the spells already existing in Golden Codebook created by fellow code-knitters
+         to enhance her chances of summoning mightier dragons.
+        </p>
+    `,
+    `
+    Page:6
+        <p>
+            Venturing into the kingdom, Kitty experienced both ecstasy and nervousness. 
+            Her love for book and her curiousness drove her to the library of the kingdom.
+             There, she devoured the books, the <b>scriptures</b> describing the spells in the Golden Codebook
+              and also discovered that even the kingdom of code-knitters
+              faced problems — there were <b>issues</b> in the codebook and the dragons could have used some
+              more finessing. She found a huge catalog of these issues.
+            
+
+        </p>`,
+        `
+        Page:7
+        <p>
+
+            Staying with the mentor, Kitty learned the intricacies of the Golden Codebook. The most shocking event
+            in her whole journey was when he told her that the no new citizen can add any new pages to the 
+            Golden Codebook.
+            How was she supposed to add spells to the Golden Codebook to summon the dragon if she could't add any
+            pages to the Golden Codebook?
+            </p>
+            <p>
+                The mentor was delighted by the brilliance of her question. The answer was that the Golden Codebook
+                already had magical pages that could hold infinite spells but the spells could be added only via the 
+                <b>Silver Codebook</b>.
+                The Silver Codebook was an exact replica of the Golden Codebook but 
+            adding magic spells there didn't really summon a dragon. However, she could create new pages in the Silver
+            Codebook and later ask the mentor or any other elders to add those spells to the Golden Book.
+            </p>
+            `,
+            `
+            Page: 8
+            <p>
+            Kitty was very glad when she received a Silver Codebook where she could make her changes but making changes in
+            Silver started becoming a bit unwieldy as there were a lots of hit and trials in the beginning. To solve that
+            problem, Kitty got an idea. She would <b>clone</b> the Silver Codebook to a <b>Wooden Codebook</b> and make changes there.
+            The wood was a perfect material to experiment upon, handy and she could carry it anywhere. 
+            When she felt satisfied with a new spell in the Wooden Codebook, she could then update
+            the Silver Codebook with those changes. 
+        </p>`,
+        `
+        Page:9
+        <p>
+            After getting the hang of the different books, Kitty wanted to focus on solving the issues of the Codeland
+            to win the favor of the elders and other fellow code-knitters.
+            Most problems were deeply magical and incomprehensible for her level, but Kitty stumbled upon 
+            a few that she could understand and solve. Excited at the prospect of obtaining 
+            her own dragon, she picked a manageable issue, consulted fellow code-knitters, and 
+            tirelessly experimented in her wooden codebook. After a long trial, it finally seemed 
+            to work, her <b>first magic spell</b>.
+        </p>`,
+        `
+        Page:10
+            <p>
+        Confident in her new spell, Kitty <b>updated the changes</b> in a new page of her Silver Codebook.
+        She then made a <b>request to merge</b> her spells to the Golden Codebook. An elder
+        meticulosly checked her spells and finally obliged to merge her request.
+        A small whirl of wind appeared above Kitty and just like that, Kitty now had her small, exquisite pet
+         <b>dragon</b>. 
+         </p>
+         <p> She was delighted but her ambitions flew higher than her pet dragon.
+             She now wanted to aim for something grander and set her sights on acquiring even bigger and more powerful dragons 
+         in the enchanting world of code-knitters. The journey of Kitty, the aspiring code-knitter,
+          had only just begun.
+        </p>`,
+    // Add more pages as needed
+];
---
+++ docs/story_page.js
@@ -0,0 +1,134 @@
+const storyPages = [
+    // Each element of this array represents a page in the story
+    `<p> 
+        This is a story about the process of making a contribution to the OSS projects.
+        <br>
+        Please hover above the bold texts to get a sense of what they mean in actual OSS projects.
+
+        <br><br><br> Click next to start the story...
+        </p>
+    `,
+    `Page:1
+    <p> 
+        Kitty couldn't believe it was a <b>machine</b> -- a huge glass sphere floating above the city, it took up as much space as the city.
+        There were some funny-looking city dwellers releasing new <b>dragons</b> into the machine, dragons of all sizes flying inside it, 
+        doing their own chores systematically to make the machine work.
+        </p>
+        <p>
+        It had everything Kitty was in love with: machines, magic and dragons. She couldn't hold back herself from exploring this strange city further.   
+        She came to know that the funny-looking people were called the <b>code-knitters</b> and they could write magical spells to summon new dragons that they
+        added to the machine to make it better.
+        Intrigued by the prospect of summoning her own <b>dragon</b>, Kitty delved into the world of <b>code-knitters</b>.
+        </p>
+        `,
+        `
+        Page:2
+        <p>
+            The code-knitters lived harmoniously in their city called "<b>Repositown</b>", knitting the spells together.  
+            Kitty also learned that these code-knitters were always on 
+            the lookout for new members to join their ranks and contribute to the summoning 
+            of even mightier dragons. Sometimes, some of the existing dragons fell ill and they could also use some help to treat them as well.
+            </p>
+            <p>
+            Each Repositown had a special book called <b>The Golden Book</b>. It contained all the spells that had summoned the dragons and the code-knitters used
+            these spells to control the dragons
+            above -- changing any spell in the Golden Book would change how the dragons behaved. A code-knitter could summon a dragon only if they were
+             able to add their magic spell to the Golden Book of the Repositown.
+        </p>`,
+        `
+        Page:3
+        <p>
+        </p>`,
+        `
+        Page:4
+        <p> 
+            Everything was new and exciting for Kitty and she wanted explore the Repositown further. All of the code-knitters were friendly and
+            she was given a <b>handbook-to-read</b> to find helpful information about the Repositown and the dragons.
+            She found out that the code-knitters had a tradition of gathering regularly in large <b>forums</b> to discuss 
+            spells, dragons, their health and other magical matters. Though warmly welcomed into the group, 
+            Kitty found it a bit challenging to grasp the complexities of their discussions. 
+            Undeterred, she decided to navigate the new city, acknowledging that some 
+            tasks would indeed be complex for her.
+        </p>
+        `,
+    `Page:5
+    <p>
+        Determined, Kitty sought the guidance of a <b>mentor</b> who taught new citizens of the city.
+        He showed her the most treasured Golden Codebook of the Repositown and guided her on the process of finding the category of spells 
+        that matched her interests.
+        Along with sharing his wisdom, he also advised her to go through the chronicles kept in the city's library.
+        Soon, Kitty was diving deep into the <b>history of the Golden Codebook</b>
+        and document outlining the <b>behaviors expected</b> in the city. He also emphasized she should try to understand the 
+        spells already existing in Golden Codebook
+         to enhance her chances of summoning mightier dragons.
+        </p>
+    `,
+    `
+    Page:6
+        <p>
+            The library had so much information about the city, the machine and the dragons. Kitty couldn't stop herself from diving in more deeper.
+             The most helpful ones for her were the <b>documents</b> describing the spells in the Golden Codebook. As she went through the spells,
+             she realized that it could be easier for her to find the mistakes in the spells rather than create new spells, at least for the beginning for 
+             her journey. But what could go so wrong in the spells that made those mighty dragons ill?
+             </p>
+             <p> 
+             Fortunately, she found a catalog of these <b>issues</b>. The issues ranged from simple to complex based on the illness of the dragons.
+             The code-knitters were thoughtful enough to categorize them so that code-knitters of all skill range could help to solve them.
+-----------------------------------------
+        </p>`,
+        `
+        Page:7
+        <p>
+
+            Staying with the mentor, Kitty learned the intricacies of the Golden Codebook. The most shocking event
+            in her whole journey was when he told her that the no new citizen can add any new pages to the 
+            Golden Codebook.
+            How was she supposed to add spells to the Golden Codebook to summon the dragon if she could't add any
+            pages to the Golden Codebook?
+            </p>
+            <p>
+                The mentor was delighted by the brilliance of her question. The answer was that the Golden Codebook
+                already had magical pages that could hold infinite spells but the spells could be added only via the 
+                <b>Silver Codebook</b>.
+                The Silver Codebook was an exact replica of the Golden Codebook but 
+            adding magic spells there didn't really summon a dragon. However, she could create new pages in the Silver
+            Codebook and later ask the mentor or any other elders to add those spells to the Golden Book.
+            </p>
+            `,
+            `
+            Page: 8
+            <p>
+            Kitty was very glad when she received a Silver Codebook where she could make her changes but making changes in
+            Silver started becoming a bit unwieldy as there were a lots of hit and trials in the beginning. To solve that
+            problem, Kitty got an idea. She would <b>clone</b> the Silver Codebook to a <b>Wooden Codebook</b> and make changes there.
+            The wood was a perfect material to experiment upon, handy and she could carry it anywhere. 
+            When she felt satisfied with a new spell in the Wooden Codebook, she could then update
+            the Silver Codebook with those changes. 
+        </p>`,
+        `
+        Page:9
+        <p>
+            After getting the hang of the different books, Kitty wanted to focus on solving the issues of the Repositown
+            to win the favor of the elders and other fellow code-knitters.
+            Most problems were deeply magical and incomprehensible for her level, but Kitty stumbled upon 
+            a few that she could understand and solve. Excited at the prospect of obtaining 
+            her own dragon, she picked a manageable issue, consulted fellow code-knitters, and 
+            tirelessly experimented in her wooden codebook. After a long trial, it finally seemed 
+            to work, her <b>first magic spell</b>.
+        </p>`,
+        `
+        Page:10
+            <p>
+        Confident in her new spell, Kitty <b>updated the changes</b> in a new page of her Silver Codebook.
+        She then made a <b>request to merge</b> her spells to the Golden Codebook. An elder
+        meticulosly checked her spells and finally obliged to merge her request.
+        A small whirl of wind appeared above Kitty and just like that, Kitty now had her small, exquisite pet
+         <b>dragon</b>. 
+         </p>
+         <p> She was delighted but her ambitions flew higher than her pet dragon.
+             She now wanted to aim for something grander and set her sights on acquiring even bigger and more powerful dragons 
+         in the enchanting world of code-knitters. The journey of Kitty, the aspiring code-knitter,
+          had only just begun.
+        </p>`,
+    // Add more pages as needed
+];
