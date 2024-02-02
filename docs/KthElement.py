e5f4adbfd4ce74469c8741dc7671bc72aba353a8
Prashant528 <prashant.tandan528@gmail.com>
2024-02-01 16:56:40 -0800

    Making the story more relatable to the actual process. Iter 2

--- docs/KthElement.py
+++ docs/KthElement.py
@@ -1,26 +1,45 @@
-def kthElement(Arr1, Arr2, k):
-    m = len(Arr1)
-    n = len(Arr2)
-    
-    if m > n:
-        return kthElement(Arr2, Arr1, k)
-    
-    if m == 0:
-        return Arr2[k - 1]
-    
-    if k == 1:
-        return min(Arr1[0], Arr2[0])
-    
-    i = min(k // 2, m)
-    j = k - i
-    
-    if Arr1[i - 1] < Arr2[j - 1]:
-        return kthElement(Arr1[i:], Arr2, k - i)
+# top down solution follows LCS recursive solution from exploration 3.3: Dynamic Programming
+def topdown_helper(dna1, dna2, m, n, memo):
+    if m < 0 or n < 0:
+        return 0
+    if memo[m][n] != 0:
+        return memo[m][n]
+    elif dna1[m] == dna2[n]:
+        length = 1 + topdown_helper(dna1, dna2, m - 1, n - 1, memo)
     else:
-        return kthElement(Arr1, Arr2[j:], k - j)
+        length = max(topdown_helper(dna1, dna2, m - 1, n, memo), topdown_helper(dna1, dna2, m, n - 1, memo))
+    memo[m][n] = length
+    return memo[m][n]
 
 
-Arr1 = [1, 2, 3, 5, 6]
-Arr2 = [3, 4, 5, 6, 7]
-k = 5
-result = kthElement(Arr1, Arr2, k)
+# receives two DNA strings and returns the length of their longest common string alignment using top-down approach
+def dna_match_topdown(DNA1, DNA2):
+    memo = [[0 for x in range(len(DNA1) + 1)] for x in range(len(DNA2) + 1)]
+    return topdown_helper(DNA1, DNA2, len(DNA1) - 1, len(DNA2) - 1, memo)
+
+
+# receives two strings, returns length of their longest common string alignment using bottom-up approach
+def dna_match_bottomup(DNA1, DNA2):
+
+    if len(DNA1) == 0 or len(DNA2) == 0:
+        return 0
+
+    # followed pseudocode from exploration 3.3: Dynamic Programming - Longest Common Subsequence Problem
+    memo = [[0 for x in range(len(DNA2) + 1)] for x in range(len(DNA1) + 1)]
+
+    for i in range(len(DNA1) + 1):
+        for j in range(len(DNA2) + 1):
+            if i == 0 or j == 0:
+                memo[i][j] = 0
+            elif DNA1[i - 1] == DNA2[j - 1]:
+                memo[i][j] = memo[i - 1][j - 1] + 1
+            else:
+                memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+
+    return memo[len(DNA2)][len(DNA1)]
+
+
+a = 'AT'
+b = 'G'
+print(dna_match_topdown(a, b))
+print(dna_match_bottomup(a, b))
--- docs/story_page.js
+++ docs/story_page.js
@@ -1,9 +1,10 @@
 const storyPages = [
     // Each element of this array represents a page in the story
     `<p> 
-        This is a story about the process of making a contribution to the OSS projects.
+        This is a story about Kitty who stumbled into a magical open source community and
+        how she goes through the process of making a contribution to the OSS projects.
         <br>
-        Please hover above the bold texts to get a sense of what they mean in actual OSS projects.
+        Please hover above the <b>bold texts in the story</b> to get a sense of what they mean in actual OSS projects.
 
         <br><br><br> Click next to start the story...
         </p>
@@ -15,7 +16,7 @@ const storyPages = [
         doing their own chores systematically to make the machine work.
         </p>
         <p>
-        It had everything Kitty was in love with: machines, magic and dragons. She couldn't hold back herself from exploring this strange city further.   
+        It had everything Kitty was in love with: machines, magic and dragons. She couldn't hold herself back from exploring this strange city further.   
         She came to know that the funny-looking people were called the <b>code-knitters</b> and they could write magical spells to summon new dragons that they
         added to the machine to make it better.
         Intrigued by the prospect of summoning her own <b>dragon</b>, Kitty delved into the world of <b>code-knitters</b>.
@@ -43,7 +44,7 @@ const storyPages = [
         Page:4
         <p> 
             Everything was new and exciting for Kitty and she wanted explore the Repositown further. All of the code-knitters were friendly and
-            she was given a <b>handbook-to-read</b> to find helpful information about the Repositown and the dragons.
+            she was given a <b>handbook-to-read</b> that gave her information about the Repositown and the dragons.
             She found out that the code-knitters had a tradition of gathering regularly in large <b>forums</b> to discuss 
             spells, dragons, their health and other magical matters. Though warmly welcomed into the group, 
             Kitty found it a bit challenging to grasp the complexities of their discussions. 
@@ -54,13 +55,13 @@ const storyPages = [
     `Page:5
     <p>
         Determined, Kitty sought the guidance of a <b>mentor</b> who taught new citizens of the city.
-        He showed her the most treasured Golden Codebook of the Repositown and guided her on the process of finding the category of spells 
-        that matched her interests.
-        Along with sharing his wisdom, he also advised her to go through the chronicles kept in the city's library.
+        He showed her to the temple that held the most treasured item of the Repositown -- the Golden Codebook and guided her 
+        on the process of finding the category of spells that matched her interests.
+        Along with sharing his wisdom, he also advised her to go through the chronicles kept in the temple's library.
         Soon, Kitty was diving deep into the <b>history of the Golden Codebook</b>
         and document outlining the <b>behaviors expected</b> in the city. He also emphasized she should try to understand the 
         spells already existing in Golden Codebook
-         to enhance her chances of summoning mightier dragons.
+        to enhance her chances of summoning mightier dragons.
         </p>
     `,
     `
@@ -68,52 +69,56 @@ const storyPages = [
         <p>
             The library had so much information about the city, the machine and the dragons. Kitty couldn't stop herself from diving in more deeper.
              The most helpful ones for her were the <b>documents</b> describing the spells in the Golden Codebook. As she went through the spells,
-             she realized that it could be easier for her to find the mistakes in the spells rather than create new spells, at least for the beginning for 
-             her journey. But what could go so wrong in the spells that made those mighty dragons ill?
+             she realized that it could be easier for her to find the mistakes in the spells rather than create new spells, at least in the beginning of 
+             her journey. She became curious about things that could go so wrong in the spells that made those mighty dragons ill.
              </p>
              <p> 
-             Fortunately, she found a catalog of these <b>issues</b>. The issues ranged from simple to complex based on the illness of the dragons.
-             The code-knitters were thoughtful enough to categorize them so that code-knitters of all skill range could help to solve them.
------------------------------------------
+             Fortunately, she found a catalog of these <b>issues</b>. She saw that issues ranged from simple to complex based on the illness of the dragons.
+             The code-knitters were thoughtful enough to categorize them so that code-knitters of all skill levels could help to solve according to their
+             ability.
+
         </p>`,
         `
         Page:7
         <p>
 
             Staying with the mentor, Kitty learned the intricacies of the Golden Codebook. The most shocking event
-            in her whole journey was when he told her that the no new citizen can add any new pages to the 
+            in her whole journey was when he told her that the no new citizen can add any new spells to the 
             Golden Codebook.
-            How was she supposed to add spells to the Golden Codebook to summon the dragon if she could't add any
-            pages to the Golden Codebook?
+            How was she supposed to correct the spells in the Golden Codebook to treat the dragons if she could't add any
+            spells to the Golden Codebook?
             </p>
-            <p>
-                The mentor was delighted by the brilliance of her question. The answer was that the Golden Codebook
-                already had magical pages that could hold infinite spells but the spells could be added only via the 
+        <p>
+                The mentor was delighted by the brilliance of her question. He reminded her how important the spells in the Golden Codebook
+                were for the proper functioning of the dragons above. New citizens of the city may not have enough expertise or may unintentionally
+                make changes that could wreak havoc on the dragons. The solution the code-knitters invented for that problem was providing each new citizen their own
                 <b>Silver Codebook</b>.
-                The Silver Codebook was an exact replica of the Golden Codebook but 
-            adding magic spells there didn't really summon a dragon. However, she could create new pages in the Silver
-            Codebook and later ask the mentor or any other elders to add those spells to the Golden Book.
+                </p>
+                <p>
+                The code-knitters called this process "forking". The Silver Codebook was created by replicating the Golden Codebook and had to remain inside 
+                the temple at all times. She could create new pages in the Silver
+                Codebook and later ask the mentor or any other elders to add those spells to the Golden Book. The elders would add them to the Golden Book only
+                after reviewing her changes deliberately.
             </p>
             `,
             `
             Page: 8
             <p>
-            Kitty was very glad when she received a Silver Codebook where she could make her changes but making changes in
-            Silver started becoming a bit unwieldy as there were a lots of hit and trials in the beginning. To solve that
+            Kitty was very glad when she received a Silver Codebook where she could make her changes but she soon realized that making changes in
+            Silver will be a bit unwieldy as it couldn't be carried outside the temple. To solve that
             problem, Kitty got an idea. She would <b>clone</b> the Silver Codebook to a <b>Wooden Codebook</b> and make changes there.
-            The wood was a perfect material to experiment upon, handy and she could carry it anywhere. 
-            When she felt satisfied with a new spell in the Wooden Codebook, she could then update
+            The wood was a perfect material to experiment upon -- handy and she could carry it anywhere. 
+            When she felt satisfied with a new spell in the Wooden Codebook, she could then <b>update</b>
             the Silver Codebook with those changes. 
         </p>`,
         `
         Page:9
         <p>
-            After getting the hang of the different books, Kitty wanted to focus on solving the issues of the Repositown
-            to win the favor of the elders and other fellow code-knitters.
-            Most problems were deeply magical and incomprehensible for her level, but Kitty stumbled upon 
-            a few that she could understand and solve. Excited at the prospect of obtaining 
-            her own dragon, she picked a manageable issue, consulted fellow code-knitters, and 
-            tirelessly experimented in her wooden codebook. After a long trial, it finally seemed 
+            After getting the hang of the different types of books, Kitty started focusing on the issues in the spells to cure the dragons.
+            Most problems were deeply magical and incomprehensible for her level, but Kitty navigated to the issues category that suited her and picked 
+            a manageable issue. She observed the dragon that had the illness and she felt immense pity that such a beautiful creature had to bear any suffering.
+            She consulted fellow code-knitters about the issue to understand it better. She, then,  
+            started to experiment in her wooden codebook tirelessly. After a long trial of changing the spell, it finally seemed 
             to work, her <b>first magic spell</b>.
         </p>`,
         `
@@ -121,14 +126,12 @@ const storyPages = [
             <p>
         Confident in her new spell, Kitty <b>updated the changes</b> in a new page of her Silver Codebook.
         She then made a <b>request to merge</b> her spells to the Golden Codebook. An elder
-        meticulosly checked her spells and finally obliged to merge her request.
-        A small whirl of wind appeared above Kitty and just like that, Kitty now had her small, exquisite pet
-         <b>dragon</b>. 
+        meticulosly checked her spells and finally obliged to <b>merge</b> her request.
+        A small whirl of wind appeared above Kitty and the dragon she had been working on came to her and paid respects.
          </p>
-         <p> She was delighted but her ambitions flew higher than her pet dragon.
-             She now wanted to aim for something grander and set her sights on acquiring even bigger and more powerful dragons 
-         in the enchanting world of code-knitters. The journey of Kitty, the aspiring code-knitter,
-          had only just begun.
+         <p> It was her first time meeting a dragon and she was filled with immense joy, a sense of fulfillment and a sense of belonging.
+            Kitty now started to feel confident that she can write her own magic spells, contribute more to the Golden Codebook and summon her own dragons.
+             The journey of Kitty, the aspiring code-knitter, had only just begun.
         </p>`,
     // Add more pages as needed
 ];
